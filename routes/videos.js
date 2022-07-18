472dda01eb5da1204f9e1c18672a2aa2af564bd4
Marco Ye <marco.ye99@gmail.com>
2022-07-17 22:07:42 -0400

    code spacing

--- routes/videos.js
+++ routes/videos.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const fs = require('fs');
 const { v4: uuid } = require('uuid');
 
-
 router.get('/', (req, res) => {
     const videosData = JSON.parse(fs.readFileSync('./data/videos.json'));
 
@@ -15,17 +14,12 @@ router.get('/:videoId', (req, res) => {
     const videosData = JSON.parse(fs.readFileSync('./data/videos.json'));
     const foundVideo = videosData.find(video => video.id === videoId);
 
-    console.log(foundVideo)
-
     res.status(200).json(foundVideo);
 })
 
 router.post('/', (req, res) => {
     const videoUserData = req.body;
-    console.log(videoUserData);
-
     const videosData = JSON.parse(fs.readFileSync('./data/videos.json'));
-
     const newVideo = {
         id: uuid(),
         title: req.body.title,
