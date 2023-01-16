262b210659f7445e0968e9c3469a06838587f82c
Marco Ye <marco.ye99@gmail.com>
2023-01-16 02:44:50 -0500

    added necessary files for netlify deployment

---
+++ functions/api.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const fs = require('fs');
+const app = express();
+const router = express.Router();
+const { v4: uuid } = require('uuid');
+
+app.use('/.netlify/functions/api', router);
+
+router.get('/test', (req, res) => {
+    res.json({
+        'test':'test'
+    });
+});
+
+router.get('/', (req, res) => {
+    const videosData = JSON.parse(fs.readFileSync('./data/videos.json'));
+
+    res.json(videosData)
+})
+
+router.get('/:videoId', (req, res) => {
+    const videoId = req.params.videoId;
+    const videosData = JSON.parse(fs.readFileSync('./data/videos.json'));
+    const foundVideo = videosData.find(video => video.id === videoId);
+
+    res.status(200).json(foundVideo);
+})
+
+router.post('/', (req, res) => {
+
+    const videosData = JSON.parse(fs.readFileSync('./data/videos.json'));
+    const newVideo = {
+        id: uuid(),
+        title: req.body.title,
+        image: 'https://i.imgur.com/FCzEBsK.jpg',
+        description: req.body.description,
+        channel: 'Michael Jackson',
+        comments: [{"name":"Billie Jean","comment":"Very cool indeed","likes":3,"timestamp": Date.now()},{"name":"Annie","comment":"I like this","likes":0,"timestamp": Date.now()}],
+        views: '1,354,685',
+        likes: '33,756',
+        timestamp: Date.now()
+    };
+
+    videosData.push(newVideo);
+
+    fs.writeFileSync('./data/videos.json', JSON.stringify(videosData));
+
+    res.json(newVideo);
+})
+
+router.post('/:videoId', (req, res) => {
+    const videoId = req.params.videoId;
+    const videosData = JSON.parse(fs.readFileSync('./data/videos.json'));
+    const foundVideo = videosData.find(video => video.id === videoId);
+    const foundComments = foundVideo.comments
+    
+    const newComment = {
+        name: "Michael Jackson", comment: req.body.comment, likes: 0, timestamp: Date.now()
+    };
+
+    foundComments.push(newComment)
+    
+    fs.writeFileSync('./data/videos.json', JSON.stringify(videosData));
+    
+    res.json(newComment);
+})
+
+module.exports = app;
+module.exports.handler = serverless(app);
